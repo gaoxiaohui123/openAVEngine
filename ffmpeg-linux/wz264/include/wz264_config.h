f7f415299bf0b5574fc5684618421894a142dea6
gaoxiaohui123 <waky_7635@126.com>
2022-06-09 15:10:38 +0800

    Delete ffmpeg-linux/wz264 directory
    safe

--- ffmpeg-linux/wz264/include/wz264_config.h
+++
@@ -1,6 +0,0 @@
-#define WZ264_GPL           1
-#define WZ264_INTERLACED    1
-#define WZ264_BIT_DEPTH     0
-#define wz264_bit_depth     0
-#define WZ264_CHROMA_FORMAT 0
-#define WZ264_VERSION "v0.8.3.0-161-gd28387b"
